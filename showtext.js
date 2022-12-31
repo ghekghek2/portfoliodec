9b12391a0f98011f2ddc4a91d965e720082dacf7
Kevin <ghekghek2@gmail.com>
2022-12-31 17:53:52 +0800

    playing with gsap scroll trigger

--- showtext.js
+++ showtext.js
@@ -1,6 +1,66 @@
-$("#mylink").mouseover(function() {
-    $("#text").fadeOut();
-  }).mouseout(function() {
+gsap.registerPlugin(ScrollTrigger);
+
+// const showtext =  document.querySelector(".showtext")
+// const littlecard =  document.querySelector(".little-card")
+// const bigcard =  document.querySelector(".big-card")
+const allvideo = document.querySelectorAll("video");
+let lastvideo = allvideo[allvideo.length - 1];
+let firstvideo = allvideo[0];
+
+console.log(lastvideo);
+
+
+
    
-    $("#text").fadeIn() ;
-  });
+    
+  
+
+gsap.to(firstvideo, {
+
+  x: 500,
+
+  duration: 3,
+ scrollTrigger:{
+  start: "top 25%",
+  trigger:firstvideo,
+  markers: true,
+  scrub: 1,
+ }
+ 
+
+})
+
+
+
+gsap.to(lastvideo, {
+  x: 900,
+
+  duration: 3,
+ scrollTrigger:{
+  start: "top 50%",
+  trigger:lastvideo,
+  markers: true,
+  scrub: 1,
+ }
+ 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// showtext.addEventListener("mouseenter", () => hover.play());
+// showtext.addEventListener("mouseleave", () => hover.reverse());
